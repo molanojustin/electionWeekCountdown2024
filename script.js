3d5237dff2b53bc612e86fd60b1939992a580d2c
molanojustin <160653314+molanojustin@users.noreply.github.com>
2024-09-21 17:36:07 -0400

    Updated time units to be displayed

--- script.js
+++ script.js
@@ -15,13 +15,25 @@ function calculateWeeksUntilElection(date) {
         const electionDate = new Date('2024-11-05');
         let inputDate = new Date(`2024-${date}`);
         const daysUntilElection = Math.ceil((electionDate - inputDate) / (24 * 60 * 60 * 1000));
-        return daysUntilElection;
+        return daysUntilElection;}
+        function calculateMoochies(date) {
+            const electionDate = new Date('2024-11-05T00:00:00');
+            let inputDate = new Date(date);
+            const minutesUntilElection = Math.ceil((electionDate - inputDate) / (60 * 1000));
+            const moochiesUntilElection = (minutesUntilElection / 15840).toFixed(2);
+            return moochiesUntilElection;
         }
 
         document.addEventListener('DOMContentLoaded', (event) => {
             const date = new Date().toISOString().slice(5, 10); // Get current date in MM-DD format
             const weeksUntilElection = calculateWeeksUntilElection(date);
             const daysUntilElection = calculateDaysUntilElection(date);
+            const currentDateTime = new Date().toISOString();
+            const moochiesUntilElection = calculateMoochies(currentDateTime);
             const countdownElement = document.getElementById('countdown');
-            countdownElement.textContent = `${weeksUntilElection} weeks to go - ${daysUntilElection} days!`;
+            const daysElement = document.getElementById('days');
+            const moochiesElement = document.getElementById('moochies');
+            countdownElement.textContent = `${weeksUntilElection} weeks`;
+            daysElement.textContent = `${daysUntilElection} days`;
+            moochiesElement.textContent = `${moochiesUntilElection} Scaramuccis`;
         });
